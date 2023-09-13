422aa9a9eefa13064a34484576e510818280d74d
Henrique Strada <henriquestradax@gmail.com>
2023-09-13 20:47:43 -0300

    finalizando js

--- javascript/main.js
+++ javascript/main.js
@@ -7,3 +7,20 @@ document.querySelector("#limpar").addEventListener("click", function (){
 document.querySelector("#deletar").addEventListener("click", function (){
     screen.value = screen.value.substring(0,(screen.value.length -1));
 });
+
+function addToScreen(value){
+    const operadoresPadrao = ["*", "+", "-", "/", "."];
+    screen.value += value;
+
+    if(operadoresPadrao.includes(value)){
+        var valorAnterior = screen.value.length -2;
+        if(operadoresPadrao.includes(screen.value[valorAnterior])){
+            screen.value = screen.value.substring(0, (screen.value.length -2));
+            screen.value = screen.value + value;
+        }
+    }
+}
+
+function resultado(){
+    screen.value = eval(screen.value);
+}
